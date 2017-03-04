63a187df4a67535c71c1dc039aab7a818bd480c1
Demven <dzmitry_salnikau@epam.com>
2017-03-04 18:49:07 -0500

    Add and configure Tslint;

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import '../../public/styles/main.pcss';
 import './app.component.pcss';
 
 @Component({
-  selector: 'my-app',
+  selector: 'ds-app',
   template: `
   <main class="AppComponent">
     <h1 class="AppComponent__heading">Hello from Angular App with Webpack</h1>
