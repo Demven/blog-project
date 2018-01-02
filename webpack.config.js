51e6263183f0510ec13441edd91e4a0a7fd33e1d
Demven <dzmitry_salnikau@epam.com>
2018-01-01 21:14:54 -0500

    Debug env values;

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ require('envkey');
 let config;
 if (process.env.NODE_ENV === 'production') {
   console.log('PRODUCTION MODE');
+  console.log('ENV VALUES:', process.env);
   config = require('./config/webpack.prod.js');
 } else {
   console.log('DEVELOPMENT MODE');
