928bb5b511e5e6d9c5acc74f28f0e6ea1757b525
Demven <dzmitry_salnikau@epam.com>
2018-01-01 22:33:39 -0500

    Remove logs from webpack.config;

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,6 @@ require('envkey');
 
 let config;
 if (process.env.NODE_ENV === 'production') {
-  console.log('PRODUCTION MODE');
-  console.log('ENV VALUES:', process.env);
   config = require('./config/webpack.prod.js');
 } else {
   console.log('DEVELOPMENT MODE');
