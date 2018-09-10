cdac18b16277c9f376a985149d98d253229216fa
demven <dmitry_salnikov@protonmail.com>
2018-09-10 00:57:06 -0400

    Debug process.env in DAL on Heroku;

--- server/server.ts
+++ server/server.ts
@@ -12,7 +12,7 @@ import { enableProdMode } from '@angular/core';
 import { ngExpressEngine } from '@nguniversal/express-engine';
 import { provideModuleMap } from '@nguniversal/module-map-ngfactory-loader';
 
-console.info('SERVER', process.env.NODE_ENV, process.env.NODE_ENV === 'production', process.env.NODE_MODULES_CACHE);
+console.info('SERVER', process.env, process.env.NODE_ENV, process.env.DYNO, process.env.JWT_SECRET);
 
 enableProdMode();
 
