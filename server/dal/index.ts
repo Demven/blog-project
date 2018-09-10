e1ba2f82631901c666eb2dc7cc3f517c5df67a22
demven <dmitry_salnikov@protonmail.com>
2018-09-10 00:15:34 -0400

    Debug process.env in DAL on Heroku;

--- server/dal/index.ts
+++ server/dal/index.ts
@@ -12,8 +12,20 @@ export default function connectToDatabase() {
   console.info('dal:', process.env); // TODO: delete
   console.info('process.env.MONGODB_USERNAME:', process.env.MONGODB_USERNAME); // TODO: delete
 
+  if (process.env.NODE_ENV === 'production') {
+    console.info('NODE_ENV: PRODUCTIIIION');
+  }
+
+  if (process.env.MONGODB_USERNAME) {
+    console.info('UUUUUUSERNAME true');
+  }
+
+  if (typeof process.env.MONGODB_USERNAME === 'string') {
+    console.info('UUUUUUSERNAME string');
+  }
+
   if (process.env.NODE_ENV === 'production' && process.env.MONGODB_USERNAME) {
-    console.info('TRUUUUUUUE');
+    console.info('all TRUUUUUUUE');
   }
 
   const options = {
